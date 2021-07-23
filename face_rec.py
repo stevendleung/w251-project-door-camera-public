d5186ebae9b6c62304a3cfcc9c4e93c3a7da5017
stevendleung <stevendleung@berkeley.edu>
2021-07-23 08:08:19 -0700

    docker file for face rec

--- face_rec.py
+++ face_rec.py
@@ -15,11 +15,11 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("steven.jpg")
+obama_image = face_recognition.load_image_file("facial_rec_images/steven.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("sarah.jpg")
+biden_image = face_recognition.load_image_file("facial_rec_images/sarah.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
