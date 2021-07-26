a53cab59a29ceb2025b0f01f4d543f0956b7cbf0
JuanOnOne <raju8901@gmail.com>
2021-07-26 14:23:44 -0400

    updating

--- docker/launch_k8s.sh
+++ docker/launch_k8s.sh
@@ -8,6 +8,7 @@ docker kill $(docker ps -q)
 
 # Launch edge k3s deployments and services
 kubectl apply -f mosquitto.yaml
+kubectl apply -f mosquittoService.yaml
 kubectl apply -f notification.yaml
 # kubectl apply -f face_detector.yaml
 # kubectl apply -f yolov5.yaml
