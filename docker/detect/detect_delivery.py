8b877819023ab8b6a6b5fd03589024bc63348562
Javed Roshan <jroshan.tx.us@gmail.com>
2021-07-31 01:11:25 -0500

    updated detect_deliver code

--- docker/detect/detect_delivery.py
+++ docker/detect/detect_delivery.py
@@ -1,7 +1,11 @@
 """Run inference with a YOLOv5 model on images
 
 Usage:
-    $ python3 path/to/detect_delivery.py --source path/to/img.jpg --weights model.pt --save-txt
+    $ python3 path/to/detect_delivery.py --source path/to/img.jpg --weights model.pt
+
+Author:
+    Javed Roshan
+    Modified the code from yolov5's detect.py
 """
 
 import argparse
@@ -13,39 +17,40 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
-# ------------------------------------------------------------------------
 # import for MQTT connectivity
 import paho.mqtt.client as mqtt
 import numpy as np
-# ------------------------------------------------------------------------
+
+import numpy as np
 
 FILE = Path(__file__).absolute()
 sys.path.append(FILE.parents[0].as_posix())  # add yolov5/ to path
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
-    apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
+from utils.augmentations import letterbox
+from utils.general import check_img_size, colorstr, non_max_suppression, scale_coords, xyxy2xywh, set_logging
+from utils.torch_utils import select_device, time_synchronized
 
-# ------------------------------------------------------------------------
+model = None        # trained model to be loaded once
+cmd_options = None  # command options to be used in the MQTT message loop
+
 # config for the MQTT messages
-LOCAL_MQTT_HOST="mosquitto-service"
+# LOCAL_MQTT_HOST="mosquitto-service"
+LOCAL_MQTT_HOST="localhost"
 LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC_IN="image_topic"
 LOCAL_MQTT_TOPIC_OUT="model_output_topic"
 connected_flag = False
-# Client declaration
+
+# setup MQTT client object
 local_mqttclient = mqtt.Client()
-cmd_options = None # used by the param parsing
 
 # callback functions for MQTT setup
 def on_connect_local(client, userdata, flags, rc): 
     global connected_flag
-    if rc == 0:      
+    if rc == 0:
         print("Successfully connected to local broker with rc: " + str(rc))
-        connected_flag = True
+        client.subscribe(LOCAL_MQTT_TOPIC_IN)
     else: 
         print("Error - Couldn't connect to local broker, rc code: " + str(rc))
 
@@ -56,166 +61,84 @@ def on_disconnect_local(client, userdata, flags, rc):
 def on_publish_local(client, userdata, msg_id):
     print("Message successfully published: {}".format(msg_id))
 
-# linking the CallBacks
-local_mqttclient.on_connect = on_connect_local
-local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
-local_mqttclient.on_publish = on_publish_local
-local_mqttclient.on_disconnect = on_disconnect_local
-
-# ------------------------------------------------------------------------
-
-@torch.no_grad()
-def run(filename, # include path of the file
+def modelLoad(
         weights='yolov5s.pt',  # model.pt path(s)
-        source='data/images',  # file/dir/URL/glob, 0 for webcam
+        source='data/images',  # not relevant with MQTT
         imgsz=640,  # inference size (pixels)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project='runs/detect',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
         ):
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
 
-    # Directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
+    global model
     # Initialize
     set_logging()
-    device = select_device(device)
+    device = select_device('')
     half &= device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
     model = attempt_load(weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check image size
-    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
     if half:
         model.half()  # to FP16
 
-    # Dataloader
-    dataset = LoadImages(source, img_size=imgsz, stride=stride)
-    bs = 1  # batch_size
-    vid_path, vid_writer = [None] * bs, [None] * bs
+@torch.no_grad()
+def run(filename, # include path of the file
+        weights='yolov5s.pt',  # model.pt path(s)
+        source='data/images',  # not relevant with MQTT
+        imgsz=640,  # inference size (pixels)
+        half=False,  # use FP16 half-precision inference
+        ):
+    global model
+    ret_msg = ''
+    device = select_device('')
 
-    # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    # Read image
+    path = source
+    path = filename
+    img0 = cv2.imread(path)  # BGR
+    im0s = img0
+    assert img0 is not None, 'Image Not Found ' + path
+
+    stride = int(model.stride.max())  # model stride
+
+    # # Padded resize
+    img = letterbox(img0, imgsz, stride=stride)[0]
+
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+
     t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
 
-        # Inference
-        t1 = time_synchronized()
-        pred = model(img,
-                     augment=augment,
-                     visualize=increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False)[0]
+    # Inference
+    t1 = time_synchronized()
+    pred = model(img, augment=False, visualize=False)[0]
 
-        # Apply NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-        t2 = time_synchronized()
+    # Apply NMS
+    pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
+    t2 = time_synchronized()
 
-        # Apply Classifier
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+        p, s, im0 = path, '', im0s.copy()
 
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+            # get the predictions to return
+            for *xyxy, conf, cls in reversed(det):
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                line = str(cls.item()) + '; ' + ','.join(map(str,xywh)) + ';'
+                ret_msg += str(line)
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                    if save_img or save_crop or view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
-                        if save_crop:
-                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
-
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
-
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                            save_path += '.mp4'
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                    vid_writer[i].write(im0)
-
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
-# ------------------------------------------------------------------------
-        #publish the message
-        msg = "files... tbd"
-        local_mqttclient.publish(LOCAL_MQTT_TOPIC, msg)
-# ------------------------------------------------------------------------
-
-    if update:
-        strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
-
-    print(f'Done. ({time.time() - t0:.3f}s)')
-    return mesg
-
+    return ret_msg
 
 # Publishing message to the Notification Queue as an output from the model
 # Message Structure:
@@ -238,6 +161,8 @@ def run(filename, # include path of the file
 def on_message(client, userdata, msg):
   '''Action to perform upon receiving message from broker. Takes image path message and runs face_rec model on that image. Publishes model output to new topic.'''
     
+    global cmd_options 
+
     # parse the input message
     new_msg = msg.payload.decode("utf-8")
     mesg_items = new_msg.split(';')
@@ -253,8 +178,7 @@ def on_message(client, userdata, msg):
     # publish the message to the notification queue
     model_output_msg = "{};{};{};{};{};{}".format(vid_source, type_of_report, classification,
                                                 person_name, current_msg, face_locations)
-    local_mqttclient.publish(LOCAL_RECEIVER_MQTT_TOPIC,model_output_msg)
-
+    local_mqttclient.publish(LOCAL_RECEIVER_MQTT_TOPIC, model_output_msg)
     return
 
 def parse_opt():
@@ -268,15 +192,20 @@ def parse_opt():
     opt = parser.parse_args()
     return opt
 
-
 def main(cmd_options):
-    print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(cmd_options).items()))
-    check_requirements(exclude=('tensorboard', 'thop'))
+    global local_mqttclient
     # Loop listener forever
+    modelLoad(**vars(cmd_options))
+
+    # linking the CallBacks
+    local_mqttclient.on_connect = on_connect_local
+    local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+    local_mqttclient.on_publish = on_publish_local
+    local_mqttclient.on_disconnect = on_disconnect_local
+    local_mqttclient.on_message = on_message
     local_mqttclient.loop_forever()
 
-
 if __name__ == "__main__":
-    print('inside __name__')
-    global cmd_options = parse_opt()
+    global cmd_options 
+    cmd_options = parse_opt()
     main(cmd_options)
