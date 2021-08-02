d08b4f910c8872ab4cd42c7402c4667d5a72cbb1
stevendleung <stevendleung@berkeley.edu>
2021-08-01 17:34:48 -0700

    integration work

--- docker/launch_k8s.sh
+++ docker/launch_k8s.sh
@@ -20,9 +20,11 @@ kubectl delete service mosquitto-service
 # Launch edge k3s deployments and services
 kubectl apply -f mosquitto/ # deployment, configmap and Service - allows across container comms
 kubectl apply -f door_cam_publisher/door_cam_publisher.yaml
+#kubectl apply -f detect/test_publisher.yaml #for using test images
 kubectl apply -f face_rec/face_rec.yaml
+kubectl apply -f detect/detect_delivery.yaml
 kubectl apply -f face_rec_listener_test/face_rec_listener_test.yaml #TEMP
-#kubectl apply -f notification/notification.yaml
+kubectl apply -f notification/notify.yaml
 # kubectl apply -f face_detector.yaml #TEMP
 # kubectl apply -f yolov5.yaml
 kubectl get pods 
